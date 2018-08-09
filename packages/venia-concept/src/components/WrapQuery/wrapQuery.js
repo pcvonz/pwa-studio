00bffda8df325eb07cba46906dba1bd9ac6ee34c
pcvonz <pzimmerman@bargreen.com>
2018-08-09 13:37:48 -0700

    Lint and fix queryArguments

--- packages/venia-concept/src/components/WrapQuery/wrapQuery.js
+++ packages/venia-concept/src/components/WrapQuery/wrapQuery.js
@@ -2,19 +2,19 @@ import React, { Component, createElement } from 'react';
 import { Query } from 'react-apollo';
 
 export default function wrapQuery(WrappedComponent, query) {
-  return class QueryHOC extends Component {
-    render() {
-      const queryArguments = this.props;
-      const props = this.props;
-      return (
-        <Query query={query} variables={{ ...queryArguments }}>
-          {({ loading, error, data }) => {
-            if (error) return <div>Data Fetch Error</div>;
-            if (loading) return <p>Fetching data...</p>;
-            return ( <WrappedComponent {...props} data={data}></WrappedComponent>)
-          }}
-        </Query>
-      )
-    }
-  }
+    return class QueryHOC extends Component {
+        render() {
+            const queryArguments = this.props.queryArguments;
+            const props = this.props;
+            return (
+                <Query query={query} variables={{ ...queryArguments }}>
+                    {({ loading, error, data }) => {
+                        if (error) return <div>Data Fetch Error</div>;
+                        if (loading) return <p>Fetching data...</p>;
+                        return <WrappedComponent {...props} data={data} />;
+                    }}
+                </Query>
+            );
+        }
+    };
 }
