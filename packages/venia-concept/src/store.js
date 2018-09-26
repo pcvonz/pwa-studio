cc28c8c8b6d80f2fd221ad17f2f858548a11f6c0
Jimmy Sanford <jimbo@users.noreply.github.com>
2018-09-26 10:00:12 -0700

    Use new Peregrine API to create store

--- packages/venia-concept/src/store.js
+++ packages/venia-concept/src/store.js
@@ -1,11 +1,22 @@
-import bootstrap from '@magento/peregrine';
+import bootstrap, { MagentoRouter } from '@magento/peregrine';
 
+import Loader from 'src/components/Loader';
+import NotFound from 'src/components/NotFound';
 import appReducer from 'src/reducers/app';
 import directoryReducer from 'src/reducers/directory';
 
-const { Provider, store } = bootstrap({
+const customRouterProps = {
     apiBase: new URL('/graphql', location.origin).toString(),
-    __tmp_webpack_public_path__: __webpack_public_path__
+    __tmp_webpack_public_path__: __webpack_public_path__,
+    CustomLoader: Loader,
+    NotFoundComponent: NotFound
+};
+
+const router = MagentoRouter;
+
+const { Provider, store } = bootstrap({
+    CustomRouter: router,
+    customRouterProps: customRouterProps
 });
 
 const { addReducer, dispatch, getState } = store;
